596a644b06efaa5d27e809a6ba6ea0f79fe87558
Thinkle <aqzlpm11@163.com>
2018-10-17 00:38:29 +0800

    reduce image size; fix some bug; refactor code to template

--- poster.py
+++ poster.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 from threading import Lock
 import json
 import time
@@ -8,28 +8,35 @@ import os
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "./upload"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['POSTER_FOLDER'] = "./static/poster/"
+app.config['POSTER_URL'] = "/static/poster/"
 
-def render_poster(user_img_file, front_img_file, output_png, user):
+def render_poster(user_img_file, front_img_file, output_file, user):
     user_img = Image.open(user_img_file)
     front_img = Image.open(front_img_file)
 
+    # 拉伸
     if user_img.size[0] > user_img.size[1]:
        sz = (front_img.size[1]/user_img.size[1]*user_img.size[0], front_img.size[1])
     else:
        sz = (front_img.size[0], front_img.size[0]/user_img.size[0]*user_img.size[1])
     sz = (int(sz[0]), int(sz[1]))
     user_img = user_img.resize(sz)
+    # 截取
     user_img = user_img.crop((0,0,front_img.size[0], front_img.size[1]))
+
+    # 灰色
     user_img = user_img.convert("I").convert("RGBA")
 
+    # 叠加前景
     img = Image.alpha_composite(user_img, front_img)
     W, H = img.size
 
+    # 加二维码
     code_img = Image.open('QRcode.png')
     img.paste(code_img, (0,0))
 
     # 加字
-
     d = ImageDraw.Draw(img)
 
     fnt = ImageFont.truetype('msyhbd.ttc', 56)
@@ -50,7 +57,12 @@ def render_poster(user_img_file, front_img_file, output_png, user):
     text_sz = fnt.getsize(user['motto'])
     d.text((40/100*W, 88.5/100*H-text_sz[1]/2), user['motto'], font=fnt, fill=(255,255,255))
 
-    img.save(output_png, "png")
+    # 减小输出大小，保存成JPG，节省空间
+    output_W = 800
+    img = img.resize((output_W, int(output_W / img.size[0] * img.size[1])))
+    img = img.convert("RGB")
+
+    img.save(output_file, "JPEG", quality=80) 
 
 
 SAVE_USER_INFO_LOCK = Lock()
@@ -62,12 +74,15 @@ def save_user_info(user):
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
-def hello():
-    return app.send_static_file('index.html')
+def home():
+    return render_template("index.html")
 
+@app.route('/show_poster/<output_file>')
+def show_poster(output_file):
+    return render_template("show_poster.html", poster_img=app.config["POSTER_URL"]+output_file)
     
 @app.route("/gen_poster",  methods=['POST'])
 def gen_poster():
@@ -88,7 +103,7 @@ def gen_poster():
 
     user_img_file = 'default-photo.png'
     front_img_file = 'front.png'
-    output_png = './static/poster/' + str(time.time()) + '.png'
+    output_file = str(time.time()) + '.jpg'
 
     if 'photo' in request.files:
         photo_file = request.files['photo']
@@ -99,35 +114,14 @@ def gen_poster():
             user_img_file = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
             photo_file.save(user_img_file)
 
-    user['photo'] = user_img_file
-    user['poster'] = output_png
+#    user['photo'] = user_img_file
+    user['poster'] = os.path.join(app.config["POSTER_FOLDER"], output_file)
     user['ip'] = request.remote_addr
     save_user_info(user)
-    render_poster(user_img_file, front_img_file, output_png, user)
-    return """
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<!-- <script src="https://cdn.jsdelivr.net/npm/vue"></script> -->
-	<link href="./static/semantic.min.css" rel="stylesheet">
-	<script src="./static/semantic.min.js"></script>
-	<title>TEDx讲者海报生成</title>
-</head>
-<body>
-<div class="ui container" style="margin-top:30px">
-    <div class="ui segment">
-	<img src='{}' style='width:100%'>
-<div class="ui one column centered grid" style="margin:7px 0 0 0">
-    长按保存图片，分享给身边的朋友
-</div>
-    </div>
-</div>
-</body>
-</html>
-""".format(output_png)
+    render_poster(user_img_file, front_img_file, os.path.join(app.config["POSTER_FOLDER"], output_file), user)
+    os.remove(user_img_file)
+    return redirect('/show_poster/{}'.format(output_file))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=36666, debug=True, threaded=True)
-    # app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=36666, threaded=True)
+#    app.run(host='0.0.0.0', port=36667, debug=True, threaded=True)
